3a3614502f127038348a19b761cf3767f1870715
frokanic <84749437+frokanic@users.noreply.github.com>
2024-08-30 10:20:39 +0300

    .toTitleCase() is an extension function that takes a phrase and uppercases the first letter of each of its words

--- src/main/kotlin/strings/StringExtensions.kt
+++ src/main/kotlin/strings/StringExtensions.kt
@@ -4,9 +4,8 @@ fun String?.isNotNullOrEmpty() = !this.isNullOrEmpty()
 
 fun String.toTitleCase(separator: String = " ") = this
     .split(separator)
-    .map { word ->
-        word[0].uppercase() + word.substring(1)
-    }
     .joinToString(
         separator = separator
-    )
+    ) { word ->
+        word[0].uppercase() + word.substring(1)
+    }
